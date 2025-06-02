f32ff2a38906483e98783273d88246498bd25558
Muhammad-Saaad <javed.iqbal12458@gmail.com>
2025-06-02 08:43:21 +0500

    Added Django Secret key, and use OS to get the secret key.

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_$u51p%!#un1$x#vmfpc_3z#)2^aq_yeu%+1@gvli8e*953x&7"
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
